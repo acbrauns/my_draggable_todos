1f4c4d71aef233154ee555fd78a19c2f630bdfff
Andrea Braunstein <acbraunstein@gmail.com>
2015-01-08 05:27:03 -0800

    sets min height for tables to half of window height

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,15 +3,16 @@ $(window).on('resize', evenOutSpacing);
 
 function evenOutSpacing(){
 
+  var windowHeight = $( window ).height();
   //get width of window, divide by 3
   var thirdWidth = $(".main").width() / 3;
 
   var tables = $(".main table");
   for (var i = 0; i < tables.length; i++){
     $(tables[i]).css("width", thirdWidth - 5); //subtract 5 for margins to avoid overflow
+    $(tables[i]).css("min-height", windowHeight/2);
   }
 
-  var windowHeight = $( window ).height();
   var mainHeight = $(".main").height();
   //must make sure div is smaller than window
   //otherwise adds negative top margin and won't scroll properly
