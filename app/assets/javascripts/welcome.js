7dd54760b43378935a37578460ae4e9d8b50dd49
Andrea Braunstein <acbraunstein@gmail.com>
2015-01-08 06:08:46 -0800

    table heights responsive to height of main div

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -4,16 +4,22 @@ $(window).on('resize', evenOutSpacing);
 function evenOutSpacing(){
 
   var windowHeight = $( window ).height();
+  $(".main").css("min-height", windowHeight/2);
+
   //get width of window, divide by 3
   var thirdWidth = $(".main").width() / 3;
-
   var tables = $(".main table");
   for (var i = 0; i < tables.length; i++){
-    $(tables[i]).css("width", thirdWidth - 5); //subtract 5 for margins to avoid overflow
+    $(tables[i]).css("width", thirdWidth - 5); //subtract for margins to avoid overflow
     $(tables[i]).css("min-height", windowHeight/2);
   }
 
+  //need to calculate mainHeight AFTER even horizontal spacing of 3 tables
   var mainHeight = $(".main").height();
+  for (var i = 0; i < tables.length; i++){
+    $(tables[i]).css("min-height", mainHeight);
+  }
+
   //must make sure div is smaller than window
   //otherwise adds negative top margin and won't scroll properly
   if (windowHeight > mainHeight){
